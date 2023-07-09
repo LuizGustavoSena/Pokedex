9bb0a0e44975c31f5faa1dd4e960d566211d951e
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-07-09 18:09:49 -0300

    test: component header

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { UserConfig, defineConfig } from 'vite';
+import { InlineConfig } from 'vitest';
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
+} as VitestConfigExport)
