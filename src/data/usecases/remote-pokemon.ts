debd6477703a3ef3ef272b97ebaeef6e580ea5a9
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-07-11 22:09:15 -0300

    feat: PokemonOnly method

--- src/data/usecases/remote-pokemon.ts
+++ src/data/usecases/remote-pokemon.ts
@@ -3,26 +3,38 @@ import { GetPokemons, Pokemons } from '../../domain/usecases';
 import { HttpClient, HttpStatusCode } from '../protocols/http';
 
 export class RemotePokemon implements GetPokemons {
-
     constructor(
         private readonly url: string,
         private readonly httpClient: HttpClient<RemotePokemon.Model>
     ) { }
+    getOnly(params: Pokemons.ParamsOnly): Promise<Pokemons.ModelOnly> {
+        throw new Error('Method not implemented.');
+    }
 
-    async get(): Promise<Pokemons.Model> {
+    async getAll(params: Pokemons.ParamsAll): Promise<Pokemons.ModelAll> {
+        const { limit } = params;
+
         let response = await this.httpClient.request({
-            url: this.url,
+            url: `${this.url}?limit=${limit}`,
             method: 'get'
         });
 
-        if (response.statusCode === HttpStatusCode.Ok)
-            return response.body as Pokemons.Model;
+        if (response.statusCode !== HttpStatusCode.Ok)
+            throw new UnexpectedError();
 
-        throw new UnexpectedError();
+        let pokemons: Pokemons.ModelOnly[] = [];
+
+        response.body?.results.forEach(el => {
+            let responsePokemon = this.httpClient.request({
+                url: el.url,
+                method: 'get'
+            })
+        })
+
     }
 
 }
 
 export namespace RemotePokemon {
-    export type Model = Pokemons.Model;
+    export type Model = Pokemons.ModelAll;
 }
--- src/domain/usecases/pokemon.ts
+++ src/domain/usecases/pokemon.ts
@@ -1,9 +1,17 @@
-import { ResponsePokemon } from "../models/pokemon-model";
+import { ResponsePokemonAll, ResponsePokemonOnly } from "../models/pokemon-model";
 
 export interface GetPokemons {
-    get(): Promise<Pokemons.Model>;
+    getAll(params: Pokemons.ParamsAll): Promise<Pokemons.ModelOnly[]>;
+    getOnly(params: Pokemons.ParamsOnly): Promise<Pokemons.ModelOnly>;
 }
 
 export namespace Pokemons {
-    export type Model = ResponsePokemon;
+    export type ParamsAll = {
+        limit: number
+    };
+    export type ParamsOnly = {
+        url: string
+    };
+    export type ModelAll = ResponsePokemonAll;
+    export type ModelOnly = ResponsePokemonOnly;
 }
