867a20a5ab0f272574540c8cab0bd16e154c4d88
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-07-21 10:27:52 -0300

    fix: home

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,39 +1,37 @@
-import { ResponsePokemonOnly } from "@/domain/models";
+import { RemotePokemon } from "@/data/usecases";
+import { Pokemons } from '@/domain/usecases';
 import Header from "@/presentation/components/header";
 import Pokemon from "@/presentation/components/pokemon";
-import faker from "faker";
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
 import style from './index.module.css';
 
-const pokemon: ResponsePokemonOnly = {
-    abilities: [{
-        ability: {
-            name: faker.name.firstName(),
-            url: faker.internet.url()
-        },
-        is_hidden: faker.random.boolean(),
-        slot: faker.random.number()
-    }],
-    height: faker.random.number(),
-    id: faker.random.number(),
-    is_default: faker.random.boolean(),
-    location_area_encounters: faker.random.words(),
-    name: faker.name.firstName(),
-    sprites: {
-        other: {
-            dream_world: {
-                front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'
-            }
-        }
-    }
+type Props = {
+    remotePokemon: RemotePokemon
 }
 
-const Home: React.FC = () => {
+const Home: React.FC<Props> = ({ remotePokemon }: Props) => {
+    const [pokemons, setPokemons] = useState<Pokemons.Model[]>();
+
+    useEffect(() => {
+        async function getPokemons() {
+            let response = await remotePokemon.getAll({
+                limit: 15
+            });
+
+            setPokemons(response);
+        };
+
+        getPokemons();
+    }, []);
+
     return (
         <>
             <Header />
             <div className={style.boxPokemon}>
-                <Pokemon item={pokemon} />
+                {pokemons?.map(pokemon => (
+                    <Pokemon item={pokemon} />
+
+                ))}
             </div>
         </>
     )
