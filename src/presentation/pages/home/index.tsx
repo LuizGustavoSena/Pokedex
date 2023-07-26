8061b3a0bbad27b611454bfb7254568811a47fa6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-07-26 09:00:42 -0300

    feat: loading component

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import { RemotePokemon } from "@/data/usecases";
 import { Pokemons } from '@/domain/usecases';
 import Header from "@/presentation/components/header";
+import Loading from "@/presentation/components/loading";
 import Pokemon from "@/presentation/components/pokemon";
 import React, { memo, useEffect, useState } from "react";
 import style from './index.module.css';
@@ -11,14 +12,19 @@ type Props = {
 
 const Home: React.FC<Props> = ({ remotePokemon }: Props) => {
     const [pokemons, setPokemons] = useState<Pokemons.Model[]>();
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
         async function getPokemons() {
+            setLoading(true);
+
             let response = await remotePokemon.getAll({
                 limit: 15
             });
 
             setPokemons(response);
+
+            setLoading(false);
         };
 
         getPokemons();
@@ -26,6 +32,7 @@ const Home: React.FC<Props> = ({ remotePokemon }: Props) => {
 
     return (
         <>
+            <Loading show={loading} />
             <Header />
             <div className={style.boxPokemon}>
                 {pokemons?.map(pokemon => (
