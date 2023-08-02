781dd1579c74e235230e51be49e09ad817a33a05
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-08-02 09:11:08 -0300

    feat: alter path

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    supportFile: 'tests/cypress/support/e2e.ts',
+    specPattern: 'tests/cypress/e2e/',
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
