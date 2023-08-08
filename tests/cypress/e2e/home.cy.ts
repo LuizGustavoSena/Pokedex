502440abfde8017d2973eccaa756e2e87dbcf426
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-08-07 22:17:23 -0300

    test: search pokemon

--- tests/cypress/e2e/home.cy.ts
+++ tests/cypress/e2e/home.cy.ts
@@ -8,4 +8,22 @@ describe('Just visit e2e test', () => {
     it('should pokemonItem component', () => {
         cy.findAllByTestId('pokemonItemComponent').should('exist');
     });
+
+    it('should correct search with lowerCase pokemonsItens', () => {
+        let pokemonSearch = 'bulbasaur'
+        cy.get('input').type(pokemonSearch);
+
+        cy.findByTestId('pokemonItemComponent')
+            .invoke('text')
+            .then(el => expect(el.toLowerCase()).to.contain(pokemonSearch));
+    });
+
+    it('should correct search with upperCase pokemonsItens', () => {
+        let pokemonSearch = 'Bulbasaur'
+        cy.get('input').type(pokemonSearch);
+
+        cy.findByTestId('pokemonItemComponent')
+            .invoke('text')
+            .then(el => expect(el).to.contain(pokemonSearch));
+    });
 })
