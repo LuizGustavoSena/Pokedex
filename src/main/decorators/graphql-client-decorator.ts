6b4fcf812eee7186f96e2924279245135011e7ea
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-08-24 21:21:54 -0300

    feat: create class GraphqlClientDecorator

---
+++ src/main/decorators/graphql-client-decorator.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpRequest, HttpResponse } from "@/data/protocols/http";
+
+export class GraphqlClientDecorator implements HttpClient {
+
+    constructor(
+        private readonly httpClient: HttpClient
+    ) { }
+
+    async request<T = any>(params: HttpRequest): Promise<HttpResponse<T>> {
+        type request = {
+            data: T
+        };
+
+        const { method, url, body, headers } = params;
+
+        let response = await this.httpClient.request<request>({
+            url,
+            method,
+            body: JSON.stringify(body),
+            headers: Object.assign(headers || {}, {
+                'Content-Type': 'application/json'
+            })
+        });
+
+        return {
+            statusCode: response.statusCode,
+            body: response.body?.data
+        }
+    }
+
+}
