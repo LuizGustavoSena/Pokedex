945a82b04f61fdcbcb3d605acc24d8b80d55571d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-08-26 11:39:30 -0300

    test: Should correct values request

--- tests/main/decorators/graphqlClientDecorator.test.ts
+++ tests/main/decorators/graphqlClientDecorator.test.ts
@@ -1,6 +1,7 @@
+import { GraphqlClientDecorator } from "@/main/decorators";
+import faker from "faker";
 import { describe, expect, it } from "vitest";
 import { HttpClientSpy, mockRequest } from "../../data/mocks";
-import { GraphqlClientDecorator } from "./graphql-client-decorator";
 
 type Props = {
     httpClientSpy: HttpClientSpy;
@@ -17,11 +18,24 @@ const makeSut = (): Props => {
 }
 
 describe('main/decorators/graphql-client-decorator', () => {
-    it('Should correct headers', () => {
+    it('Should correct values request', async () => {
         const { httpClientSpy, sut } = makeSut();
 
-        sut.request(mockRequest());
+        httpClientSpy.response = {
+            statusCode: faker.random.number(),
+            body: { data: faker.random.objectElement() }
+        }
 
-        expect(httpClientSpy.headers).toEqual({ 'Content-Type': 'application/json' })
-    })
+        let request = mockRequest();
+
+        let response = await sut.request(request);
+
+        expect(httpClientSpy.url).toBe(request.url);
+        expect(httpClientSpy.body).toBe(JSON.stringify(request.body));
+        expect(httpClientSpy.method).toBe(request.method);
+        expect(response.statusCode).toBe(httpClientSpy.response.statusCode);
+        expect(response.body).toBe(httpClientSpy.response.body.data);
+    });
+
+
 })
